0c350c53d0431abb3dfe760aad8a6f7ec755e340
Vsevolod Romashov <7@7vn.ru>
2015-10-13 01:25:14 +0300

    initial commit

---
+++ lib/labourant.rb
@@ -0,0 +1,21 @@
+require 'telegram/bot'
+
+module Labourant
+  class << self
+    def start
+      Telegram::Bot::Client.run(ENV['TELEGRAM_TOKEN'], logger: Logger.new(STDOUT)) do |bot|
+        bot.listen do |message|
+          case message.text
+          when '/start'
+            bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+          when '/stop'
+            bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+          when '/wat'
+            file_id = 'AgADAgADqacxGyi26Afsf6Keo16E3KZjhCoABHn6hNmneqljmboAAgI'
+            bot.api.send_photo(chat_id: message.chat.id, photo: file_id)
+          end
+        end
+      end
+    end
+  end
+end
