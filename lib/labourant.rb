cc804347957d54303c702ead8210bf345cc09608
Vsevolod Romashov <7@7vn.ru>
2015-10-26 02:08:41 +0300

    add search results pagination with /more command

--- lib/labourant.rb
+++ lib/labourant.rb
@@ -7,34 +7,77 @@ module Labourant
 
   class << self
     def start
-      Telegram::Bot::Client.run(ENV['TELEGRAM_TOKEN'], logger: Logger.new(STDOUT)) do |bot|
-        bot.listen do |message|
-          case message.text
-          when %r(/jobs (?<keywords>.*$))
-            keywords = Regexp.last_match[:keywords]
-            bot.api.send_message(chat_id: message.chat.id, text: "Searching for jobs with '#{keywords}'...")
+      telegram.listen do |message|
+        case message.text
+        when %r(/jobs (?<keywords>.*$))
+          keywords = Regexp.last_match[:keywords]
+          last_searches[message.chat.id] = [keywords, 0]
+          telegram.api.send_message(chat_id: message.chat.id, text: "Searching for jobs with '#{keywords}'...")
 
-            response = upwork.find_jobs(q: keywords, limit: LIMIT)
-            total_found = response['paging']['total']
-            offers = response['jobs']
-            bot.api.send_message(chat_id: message.chat.id, text: "Showing first #{offers.count} of #{total_found} jobs:")
+          response = upwork.find_jobs(q: keywords, limit: LIMIT)
 
-            offers.each do |offer|
-              bot.api.send_message(
+          if response['jobs'].empty?
+            telegram.api.send_message(chat_id: message.chat.id, text: 'No jobs found')
+          else
+            telegram.api.send_message(
+              chat_id: message.chat.id,
+              text: "Showing 1..#{response['jobs'].count} of #{response['paging']['total']} jobs:"
+            )
+
+            response['jobs'].each do |offer|
+              telegram.api.send_message(
                 chat_id: message.chat.id,
                 text: "[#{offer['title']}](#{offer['url']}) | #{offer['skills'].join(', ')}",
                 parse_mode: 'Markdown'
               )
             end
-          else
-            file_id = 'AgADAgADqacxGyi26Afsf6Keo16E3KZjhCoABHn6hNmneqljmboAAgI'
-            bot.api.send_photo(chat_id: message.chat.id, photo: file_id)
           end
+        when '/more'
+          if last_search = last_searches[message.chat.id]
+            new_offset = last_search.last + LIMIT
+            last_searches[message.chat.id] = [last_search.first, new_offset]
+            telegram.api.send_message(chat_id: message.chat.id, text: "Searching for more jobs with '#{last_search.first}'...")
+
+            response = upwork.find_jobs(q: last_search.first, limit: LIMIT, offset: new_offset)
+
+            if response['jobs'].empty?
+              telegram.api.send_message(chat_id: message.chat.id, text: 'No jobs found')
+            else
+              telegram.api.send_message(
+                chat_id: message.chat.id,
+                text: "Showing #{new_offset + 1}..#{new_offset + response['jobs'].count} of #{response['paging']['total']} jobs:"
+              )
+
+              response['jobs'].each do |offer|
+                telegram.api.send_message(
+                  chat_id: message.chat.id,
+                  text: "[#{offer['title']}](#{offer['url']}) | #{offer['skills'].join(', ')}",
+                  parse_mode: 'Markdown'
+                )
+              end
+            end
+          else
+            telegram.api.send_message(
+              chat_id: message.chat.id,
+              text: 'Use /jobs command first.'
+            )
+          end
+        else
+          file_id = 'AgADAgADqacxGyi26Afsf6Keo16E3KZjhCoABHn6hNmneqljmboAAgI'
+          telegram.api.send_photo(chat_id: message.chat.id, photo: file_id)
         end
       end
     end
 
   private
+    def last_searches
+      @last_searches ||= {}
+    end
+
+    def telegram
+      Telegram::Bot::Client.new(ENV['TELEGRAM_TOKEN'], logger: Logger.new(STDOUT))
+    end
+
     def upwork
       @upwork ||= Upwork.new
     end
