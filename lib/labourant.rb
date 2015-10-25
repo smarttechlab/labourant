59089e819b76d57ca10498a8218d4db396638ba7
Vsevolod Romashov <7@7vn.ru>
2015-10-25 23:22:46 +0300

    add basic jobs search

--- lib/labourant.rb
+++ lib/labourant.rb
@@ -1,22 +1,42 @@
 require 'telegram/bot'
 require_relative 'labourant/upwork'
+require 'awesome_pry'
 
 module Labourant
+  LIMIT = 5
+
   class << self
     def start
       Telegram::Bot::Client.run(ENV['TELEGRAM_TOKEN'], logger: Logger.new(STDOUT)) do |bot|
         bot.listen do |message|
           case message.text
-          when '/start'
-            bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-          when '/stop'
-            bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-          when '/wat'
+          when %r(/jobs (?<keywords>.*$))
+            keywords = Regexp.last_match[:keywords]
+            bot.api.send_message(chat_id: message.chat.id, text: "Searching for jobs with '#{keywords}'...")
+
+            response = upwork.find_jobs(q: keywords, limit: LIMIT)
+            total_found = response['paging']['total']
+            offers = response['jobs']
+            bot.api.send_message(chat_id: message.chat.id, text: "Showing first #{offers.count} of #{total_found} jobs:")
+
+            offers.each do |offer|
+              bot.api.send_message(
+                chat_id: message.chat.id,
+                text: "[#{offer['title']}](#{offer['url']}) | #{offer['skills'].join(', ')}",
+                parse_mode: 'Markdown'
+              )
+            end
+          else
             file_id = 'AgADAgADqacxGyi26Afsf6Keo16E3KZjhCoABHn6hNmneqljmboAAgI'
             bot.api.send_photo(chat_id: message.chat.id, photo: file_id)
           end
         end
       end
+    end
+
+  private
+    def upwork
+      @upwork ||= Upwork.new
     end
   end
 end
