aa9c8c9d6c0454d47dcc96bd95b366de3b6983ff
Vsevolod Romashov <7@7vn.ru>
2015-10-28 23:00:40 +0300

    add basic authorization

--- lib/labourant.rb
+++ lib/labourant.rb
@@ -10,26 +10,30 @@ module Labourant
       telegram.listen do |message|
         user_id = message.chat.id
 
-        case message.text
-        when %r(/jobs (?<keywords>.*$))
-          keywords = Regexp.last_match[:keywords]
-          last_searches[user_id] = [keywords, 0]
-          send_message("Searching for jobs with '#{keywords}'...", to: user_id)
+        if authorized_uids.include?(user_id)
+          case message.text
+          when %r(/jobs (?<keywords>.*$))
+            keywords = Regexp.last_match[:keywords]
+            last_searches[user_id] = [keywords, 0]
+            send_message("Searching for jobs with '#{keywords}'...", to: user_id)
 
-          find_and_send_jobs(keywords, 0, user_id)
-        when '/more'
-          if last_search = last_searches[user_id]
-            new_offset = last_search.last + LIMIT
-            last_searches[user_id] = [last_search.first, new_offset]
-            send_message("Searching for more jobs with '#{last_search.first}'...", to: user_id)
+            find_and_send_jobs(keywords, 0, user_id)
+          when '/more'
+            if last_search = last_searches[user_id]
+              new_offset = last_search.last + LIMIT
+              last_searches[user_id] = [last_search.first, new_offset]
+              send_message("Searching for more jobs with '#{last_search.first}'...", to: user_id)
 
-            find_and_send_jobs(last_search.first, new_offset, user_id)
+              find_and_send_jobs(last_search.first, new_offset, user_id)
+            else
+              send_message('Use /jobs command first.', to: user_id)
+            end
           else
-            send_message('Use /jobs command first.', to: user_id)
+            file_id = 'AgADAgADqacxGyi26Afsf6Keo16E3KZjhCoABHn6hNmneqljmboAAgI'
+            telegram.api.send_photo(chat_id: user_id, photo: file_id)
           end
         else
-          file_id = 'AgADAgADqacxGyi26Afsf6Keo16E3KZjhCoABHn6hNmneqljmboAAgI'
-          telegram.api.send_photo(chat_id: user_id, photo: file_id)
+          send_message('You are not allowed to use this bot', to: user_id)
         end
       end
     end
@@ -57,6 +61,10 @@ module Labourant
 
     def last_searches
       @last_searches ||= {}
+    end
+
+    def authorized_uids
+      ENV['AUTHORIZED_UIDS'].split(',').map(&:to_i)
     end
 
     def telegram
